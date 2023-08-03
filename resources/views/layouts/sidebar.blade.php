fe980b9090b43adff460b3ac89ca8e2db504756d
Faruzaa <ahmadfaza200@gmail.com>
2023-08-03 16:43:56 +0700

    membuat custom scrollbar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
       <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
       </div>
-      <div class="sidebar-brand-text mx-3">SiMagang</div>
+      <div class="sidebar-brand-text mx-3">S<span class="text-lowercase">i</span>M<span class="text-lowercase">agAng</span></div>
   </a>
 
   <!-- Divider -->
