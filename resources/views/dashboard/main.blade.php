1195a154cd41134e17ee5dad3f816ad1f24402d7
Faruzaa <ahmadfaza200@gmail.com>
2023-08-04 20:39:54 +0700

    (lanjutan commit sebelumnya)

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -12,7 +12,7 @@
                 <div class="col mr-2">
                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                         Jumlah Barang</div>
-                    <div class="h5 mb-0 font-weight-bold text-gray-800">1287</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $stok }}</div>
                 </div>
                 <div class="col-auto">
                     <i class="fas fa-box-open fa-2x text-gray-300"></i>
