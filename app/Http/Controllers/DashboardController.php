7ece6f8d94cdb9ac8e1ff33f4938265d4c7784b1
Faruzaa <ahmadfaza200@gmail.com>
2023-08-04 20:39:03 +0700

    menambahkan jumlah stok

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\products;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     //
     function index(){
-        return view('dashboard.main');
+        $stok = products::count();
+        return view('dashboard.main', compact('stok'));
     }
 }
